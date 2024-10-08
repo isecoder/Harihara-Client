bfcbcb5ece7a0644106c60ec98641aad8089bc38
Sudheesh Patwardhan <patwardhansudheesh@gmail.com>
2024-10-08 21:59:02 +0530

    Initial2

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,7 +4,7 @@ import './MainContent.css';
 const MainContent = () => {
   return (
     <div className="main-content">
-      <h2>Launching Soon</h2>
+      <h2 className="launching-soon">Launching Soon</h2>
       <p>Our new website is under construction. Stay tuned for something amazing!</p>
     </div>
   );
