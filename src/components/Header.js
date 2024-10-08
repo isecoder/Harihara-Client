4c860564a1aaaa14443c4c19bdabb7645f4f1150
Shreekara B <shreekarab5@gmail.com>
2024-10-08 22:34:38 +0530

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,23 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header className="header">
-      <h1>Harihareshwara Temple</h1>
-      <nav>
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+    <header className="banner">
+      <div className="header-left">
+        <img src='../assets/harihareshwara.jpg' alt="God Image" className="god-image" />
+      </div>
+      <div className="header-center">
+        <div className="gov-info">
+          <span>Government of Karnataka</span>
+          <span>Hindu Religious Institutions & Charitable Endowments Department</span>
+        </div>
+        <div className="temple-info">
+          <h1>Shri Harihareshwara Temple</h1>
+          <p>Hariharapallathadka, Dakshina Kannada - 574218</p>
+        </div>
+      </div>
+      <div className="header-right">
+        <img src="C:\Users\shree\Downloads\Harihara-Client\src\assets\Om.png" alt="Om Symbol" className="om-symbol" />
+      </div>
     </header>
   );
 };
